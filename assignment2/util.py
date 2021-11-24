3e9431236bb8e033f29743d28f454997b1c1ed40
stto <sspjyins@snu.ac.kr>
2021-11-24 21:21:54 +0900

    init assignment2

---
+++ assignment2/util.py
@@ -0,0 +1,20 @@
+import copy
+import hashlib
+import matplotlib.pyplot as plt
+import random
+from collections import deque
+
+WALL = '#'
+GHOST = 'G'
+POWER = 'P'
+ITEM = '.'
+BLANK = ' '
+USER = 'U'
+SCORE_NORMAL = 10
+SCORE_ACTION = -1
+SCORE_POWER = 100
+SCORE_CATCH = 200
+SCORE_DEAD = -500
+SCORE_ALIVE = 500
+TOTAL_EPISODE = 1000
+TEST_STEP = 10
